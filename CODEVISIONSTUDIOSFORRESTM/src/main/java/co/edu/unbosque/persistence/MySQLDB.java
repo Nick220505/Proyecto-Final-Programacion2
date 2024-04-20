edbb296c3aca76869485404410f09130fa8dcf70
Nico <juannicolaspardo@gmail.com>
2024-04-20 18:45:45 -0500

    Capa de pesistencia actualizada.

--- CODEVISIONSTUDIOSFORRESTM/src/main/java/co/edu/unbosque/persistence/MySQLDB.java
+++ CODEVISIONSTUDIOSFORRESTM/src/main/java/co/edu/unbosque/persistence/MySQLDB.java
@@ -1,53 +1,40 @@
 package co.edu.unbosque.persistence;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Optional;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class MySQLDB<T extends Object> {
 
-	private final String cxString = "jdbc:mysql://localhost:3306/forrestm?serverTimezone=UTC&useSSL=false";
-	private final String user = "root";
-	private final String pass = "";
-	private Connection con = null;
-	private Statement stmt;
-	private ResultSet rs;
+	private Class<T> clase;
 
-	public void conectar() throws Exception {
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		con = DriverManager.getConnection(cxString, user, pass);
+	public MySQLDB(Class<T> clase) {
+		this.clase = clase;
 	}
 
-	public void insertarDB(String cadena) throws Exception {
-		conectar();
-		stmt = con.createStatement();
-		stmt.executeUpdate(cadena);
-		con.close();
+	private SessionFactory crearSessionFactory() {
+		return new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(clase)
+				.buildSessionFactory();
 	}
-	
-	public Optional<T> consultarFilaDB(String cadena, RowMapper<T> rm) throws Exception {
-		T objeto = null;
-		conectar();
-		stmt = con.createStatement();
-		rs = stmt.executeQuery(cadena);
-		if (rs.next()) {
-			objeto = rm.mapRow(rs);
+
+	public void ejecutarTransaccion(SessionMapper<T> sm) throws Exception {
+		try (SessionFactory miFactory = crearSessionFactory();
+			 Session miSession = miFactory.openSession();) {
+			miSession.beginTransaction();
+			sm.mapSession(miSession);
+			miSession.getTransaction().commit();
 		}
-		return Optional.ofNullable(objeto);
 	}
-	
-	public ArrayList<T> consultarTablaDB(String cadena, RowMapper<T> rm) throws Exception {
-		ArrayList<T> listaObjetos = new ArrayList<>();
-		conectar();
-		stmt = con.createStatement();
-		rs = stmt.executeQuery(cadena);
-		while (rs.next()) {
-			T objeto = rm.mapRow(rs);
-			listaObjetos.add(objeto);
+
+	public List<T> ejecutarConsulta(String consulta) throws Exception {
+		try (SessionFactory miFactory = crearSessionFactory();
+			 Session miSession = miFactory.openSession();) {
+			return miSession.createQuery(consulta, clase).getResultList();
 		}
-		return listaObjetos;
 	}
+
 }
--- CODEVISIONSTUDIOSFORRESTM/src/main/java/co/edu/unbosque/persistence/RowMapper.java
+++
@@ -1,8 +0,0 @@
-package co.edu.unbosque.persistence;
-
-import java.sql.ResultSet;
-
-@FunctionalInterface
-public interface RowMapper<T extends Object> {
-	public abstract T mapRow(ResultSet rs) throws Exception;
-}
