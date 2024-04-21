41706fe25b1c8d67c0a92f64ac17b76e21744e4f
Nico <juannicolaspardo@gmail.com>
2024-04-21 01:36:25 -0500

    ModoTransmisionDAO y TipoMusicaDAO actualizados.

--- CODEVISIONSTUDIOSFORRESTM/src/main/java/co/edu/unbosque/dao/ModoTransmisionDAO.java
+++ CODEVISIONSTUDIOSFORRESTM/src/main/java/co/edu/unbosque/dao/ModoTransmisionDAO.java
@@ -1,11 +1,28 @@
 package co.edu.unbosque.dao;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import co.edu.unbosque.dto.ModoTransmisionDTO;
 
 public class ModoTransmisionDAO extends Gestion<ModoTransmisionDTO> {
 
 	public ModoTransmisionDAO() {
 		super(ModoTransmisionDTO.class);
+	}
+	
+	public List<String> listarModosDeTransmision() throws Exception {
+		asignarModosDeTransmision();
+		return listar().stream().map(ModoTransmisionDTO::getModo).collect(Collectors.toList());
+	}
+	
+	public void asignarModosDeTransmision() throws Exception {
+		String[] modos = {"AM", "FM", "Streaming"};
+		for (String modo : modos) {
+			if (!listar().stream().map(ModoTransmisionDTO::getModo).collect(Collectors.toList()).contains(modo)) {
+				agregar(new ModoTransmisionDTO(modo));
+			}
+		}
 	}
 
 }
--- CODEVISIONSTUDIOSFORRESTM/src/main/java/co/edu/unbosque/dao/TipoMusicaDAO.java
+++ CODEVISIONSTUDIOSFORRESTM/src/main/java/co/edu/unbosque/dao/TipoMusicaDAO.java
@@ -1,11 +1,28 @@
 package co.edu.unbosque.dao;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import co.edu.unbosque.dto.TipoMusicaDTO;
 
 public class TipoMusicaDAO extends Gestion<TipoMusicaDTO> {
 
 	public TipoMusicaDAO() {
 		super(TipoMusicaDTO.class);
+	}
+	
+	public List<String> listarTiposDeMusica() throws Exception {
+		asignarTiposDeMusica();
+		return listar().stream().map(TipoMusicaDTO::getTipo).collect(Collectors.toList());
+	}
+	
+	public void asignarTiposDeMusica() throws Exception {
+		String[] tipos = {"Rock", "De Planchar", "Reggaeton"};
+		for (String tipo : tipos) {
+			if (!listar().stream().map(TipoMusicaDTO::getTipo).collect(Collectors.toList()).contains(tipo)) {
+				agregar(new TipoMusicaDTO(tipo));
+			}
+		}
 	}
 
 }
