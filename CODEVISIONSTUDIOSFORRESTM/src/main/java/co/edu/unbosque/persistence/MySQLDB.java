e5c74892f10aede1e40d773478df425814a88fbd
Nico <juannicolaspardo@gmail.com>
2024-04-21 15:20:47 -0500

    Declaración de la clase MySQLDB actualizada.

--- CODEVISIONSTUDIOSFORRESTM/src/main/java/co/edu/unbosque/persistence/MySQLDB.java
+++ CODEVISIONSTUDIOSFORRESTM/src/main/java/co/edu/unbosque/persistence/MySQLDB.java
@@ -6,7 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class MySQLDB<T extends Object> {
+public class MySQLDB<T> {
 
 	private Class<T> clase;
 
