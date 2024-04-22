f499598f09099831c1c4c35dadc448ef11103c9c
aulainformatica <aulainformatica@UBSCOA402-40.unbosque.edu.co>
2024-04-22 10:00:25 -0500

    Página de creación de canción actualizada.

--- CODEVISIONSTUDIOSFORRESTM/src/main/java/co/edu/unbosque/model/CreacionCancionBean.java
+++ CODEVISIONSTUDIOSFORRESTM/src/main/java/co/edu/unbosque/model/CreacionCancionBean.java
@@ -2,12 +2,39 @@ package co.edu.unbosque.model;
 
 import javax.faces.bean.ManagedBean;
 
+import co.edu.unbosque.dao.ArtistaDAO;
+import co.edu.unbosque.dao.ArtistaYCancionDAO;
+import co.edu.unbosque.dao.CancionDAO;
+
 @ManagedBean
 public class CreacionCancionBean {
+	
+	private CancionDAO gestorCanciones;
+	private ArtistaDAO gestorArtistas;
+	private ArtistaYCancionDAO gestorCancionesYArtistas;
 	
 	private String nombre;
 	private String artista;
 	private String url;
+	
+	public CreacionCancionBean() {
+		gestorCanciones = new CancionDAO();
+		gestorArtistas = new ArtistaDAO();
+		gestorCancionesYArtistas = new ArtistaYCancionDAO();
+	}
+	
+	public String enviar() {
+		
+		
+		
+		return "paginaInicio.xhtml";
+	}
+	
+	public void reiniciarEntradas() {
+		setNombre("");
+		setArtista("");
+		setUrl("");
+	}
 	
 	public String getNombre() {
 		return nombre;
