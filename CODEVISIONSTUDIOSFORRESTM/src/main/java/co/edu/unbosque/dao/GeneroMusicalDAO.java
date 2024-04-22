b3786713f132e3a820eb8579c5866dddf609ee2d
aulainformatica <aulainformatica@UBSCOA402-40.unbosque.edu.co>
2024-04-22 10:25:13 -0500

    Asignación y obtención de generos musicales agregados.

--- CODEVISIONSTUDIOSFORRESTM/src/main/java/co/edu/unbosque/dao/GeneroMusicalDAO.java
+++ CODEVISIONSTUDIOSFORRESTM/src/main/java/co/edu/unbosque/dao/GeneroMusicalDAO.java
@@ -1,11 +1,31 @@
 package co.edu.unbosque.dao;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import co.edu.unbosque.dto.GeneroMusicalDTO;
 
 public class GeneroMusicalDAO extends Gestion<GeneroMusicalDTO> {
 
 	public GeneroMusicalDAO() {
 		super(GeneroMusicalDTO.class);
+	}
+
+	public List<String> listarGenerosMusicales() throws Exception {
+		return listar().stream().map(GeneroMusicalDTO::getGenero).collect(Collectors.toList());
+	}
+
+	public void asignarGenerosMusicales() throws Exception {
+		List<String> generosExistentes = listar().stream().map(GeneroMusicalDTO::getGenero).collect(Collectors.toList());
+		Set<String> generosAAgregar = new HashSet<>(Arrays.asList("Rock", "Reggaeton", "Pop"));
+		generosAAgregar.removeAll(generosExistentes);
+
+		for (String genero : generosAAgregar) {
+			agregar(new GeneroMusicalDTO(genero));
+		}
 	}
 
 }
