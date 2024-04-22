3b5eb8614d911604b0c4f2c91e8186e6f9a1434f
aulainformatica <aulainformatica@UBSCOA402-40.unbosque.edu.co>
2024-04-22 09:44:23 -0500

    CreacionCancionBean agregado.

---
+++ CODEVISIONSTUDIOSFORRESTM/src/main/java/co/edu/unbosque/model/CreacionCancionBean.java
@@ -0,0 +1,36 @@
+package co.edu.unbosque.model;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class CreacionCancionBean {
+	
+	private String nombre;
+	private String artista;
+	private String url;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getArtista() {
+		return artista;
+	}
+	
+	public void setArtista(String artista) {
+		this.artista = artista;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+}
