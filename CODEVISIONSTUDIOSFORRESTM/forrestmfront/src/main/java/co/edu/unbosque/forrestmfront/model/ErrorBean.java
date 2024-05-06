24020a915de0500745b7555a907c8b39fa13cbcc
Nico <juannicolaspardo@gmail.com>
2024-05-05 23:47:46 -0500

    Warnings de favicons eliminados.

--- CODEVISIONSTUDIOSFORRESTM/forrestmfront/src/main/java/co/edu/unbosque/forrestmfront/model/ErrorBean.java
+++ CODEVISIONSTUDIOSFORRESTM/forrestmfront/src/main/java/co/edu/unbosque/forrestmfront/model/ErrorBean.java
@@ -6,11 +6,12 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ComponentSystemEvent;
 
 @ManagedBean(name = "errorBean")
 @RequestScoped
 public class ErrorBean {
-	public void redirigirAPaginaInicio() {
+	public void redirigirAPaginaInicio(ComponentSystemEvent event) {
 		FacesContext facesContext = FacesContext.getCurrentInstance();
 		ExternalContext externalContext = facesContext.getExternalContext();
 		if (externalContext.getSessionMap().get("mensajeError") == null) {
